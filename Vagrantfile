b1bf696743781efc9c46df5601094c3f36d8c07c
crwgregory <crwgregory@gmail.com>
2016-05-04 08:31:46 -0600

    Updates to bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,11 @@ Vagrant.configure("2") do |config|
     # The ip address you want to use to connect
     config.vm.network :private_network, ip: "192.168.55.101"
 
+    # 1gb memory
+    config.vm.provider :virtualbox do |v|
+          v.customize ["modifyvm", :id, "--memory", 1024]
+    end
+
     # Load in our script file
     config.vm.provision :shell, path: "bootstrap.sh"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y git
 
 # GIT CONFIG
-git config --global user.name "Your Name"
+git config --global user.name "Your Git Hub Name"
 git config --global user.email "your@email.com"
 
 # RUBY VERSION MANAGER
@@ -13,7 +13,9 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 \curl -sSL https://get.rvm.io | bash -s stable
 
 # WHATEVER VERSION OF RUBY YOU WANT TO USE AT START UP
-# sudo rvm install 2.1.5
+rvm install 2.1.5
 
-# CREATE A GEMSET
-# rvm use --create 2.1.5@yourproject
+# RUN THESE COMMANDS FROM INSIDE VAGRANT SSH SHELL TO CREATE A GEM SET.
+# sudo -i
+# rvm use 2.1.5@yourproject --create
+# exit
