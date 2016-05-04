e24c6b25cc0d8838ed9d79ea7adbf26175d30d42
crwgregory <crwgregory@gmail.com>
2016-05-04 08:15:54 -0600

    RVM Install

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,10 @@ Vagrant.configure("2") do |config|
 
     config.vm.box = "ubuntu/trusty64"
 
+    # The ip address you want to use to connect
+    config.vm.network :private_network, ip: "192.168.55.101"
+
+    # Load in our script file
     config.vm.provision :shell, path: "bootstrap.sh"
 
     config.vm.synced_folder "./www", "/vagrant/www", { :mount_options => ['dmode=777','fmode=777'] }
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,16 @@ sudo apt-get update
 
 sudo apt-get install -y git
 
+# GIT CONFIG
 git config --global user.name "Your Name"
 git config --global user.email "your@email.com"
+
+# RUBY VERSION MANAGER
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
+# WHATEVER VERSION OF RUBY YOU WANT TO USE AT START UP
+# sudo rvm install 2.1.5
+
+# CREATE A GEMSET
+# rvm use --create 2.1.5@yourproject
